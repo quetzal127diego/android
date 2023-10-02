4ebc121a5563a45dc22dbee2af5dbd520759f791
QuetzalMF <quetzald127@gmail.com>
2023-10-02 12:49:35 -0600

    New CountDownTimer

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,46 +1,49 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.os.CountDownTimer;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity {
 
-    //Button B;
+    private TextView countdownTextView;
+    private CountDownTimer countDownTimer;
 
-    Button B1;
-
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        B1 = (Button) findViewById(R.id.btn1);
-        B1.setOnClickListener(this);
+        countdownTextView = findViewById(R.id.countdown_textview);
 
+        startCountdownTimer();
+    }
+
+    private void startCountdownTimer() {
+        countDownTimer = new CountDownTimer(5000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                // Actualizar el TextView con el tiempo restante
+                int secondsRemaining = (int) (millisUntilFinished / 1000);
+                countdownTextView.setText(String.valueOf(secondsRemaining));
+            }
+
+            @Override
+            public void onFinish() {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+                finish();
+            }
+        }.start();
     }
 
     @Override
-    public void onClick(View view) {
-
-        startActivity(new Intent(this, MainActivity2.class));
-
+    protected void onDestroy() {
+        super.onDestroy();
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+        }
     }
-
-    /* @Override
-    protected void onStart() {
-        super.onStart();
-        B1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setContentView(R.layout.activity_main2);
-            }
-        });
-    }*/
 }
