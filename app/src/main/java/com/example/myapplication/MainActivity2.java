16da301bbc84750ca6b0f67190526115001b8d7f
QuetzalMF <quetzald127@gmail.com>
2023-10-01 22:08:09 -0600

    Commite add explicits botons

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void abrirNavegador(View v)
+    {
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://developer.android.com/?hl=es-419"));
+        startActivity(i);
+    }
+
+    public void abrirMapa(View v)
+    {
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://maps.app.goo.gl/QqmPf8r1Q7Au3fvx5"));
+        startActivity(i);
+    }
+
+    public void mostratContactos(View v)
+    {
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("content://contacts/people"));
+        startActivity(i);
+    }
+
+    public void abrirCamara (View v)
+    {
+        Intent i = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivity(i);
     }
 }
