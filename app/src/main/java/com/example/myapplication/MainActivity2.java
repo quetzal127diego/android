2e0fe0e4b830a6e014679d36889151bba3359a09
QuetzalMF <quetzald127@gmail.com>
2023-10-02 13:05:28 -0600

    New buttons

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -38,4 +38,33 @@ public class MainActivity2 extends AppCompatActivity {
         Intent i = new Intent("android.media.action.IMAGE_CAPTURE");
         startActivity(i);
     }
+
+    public void abrirSegundaActividad(View v) {
+        Intent i = new Intent(this, MainActivity3.class); // Reemplaza "SegundaActividad" con el nombre de tu segunda actividad
+        startActivity(i);
+    }
+
+    public void enviarDatos(View v) {
+        Intent i = new Intent(this, MainActivity3.class); // Reemplaza "OtraActividad" con el nombre de tu otra actividad
+        i.putExtra("clave", "valor"); // Puedes agregar datos extras aquí
+        startActivity(i);
+    }
+
+    public void realizarLlamada(View v) {
+        Intent i = new Intent(Intent.ACTION_DIAL);
+        i.setData(Uri.parse("tel:123456789")); // Reemplaza "123456789" con el número de teléfono que deseas llamar
+        startActivity(i);
+    }
+
+    public void enviarCorreo(View v) {
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("message/rfc822");
+        i.putExtra(Intent.EXTRA_EMAIL, new String[]{"diegomercado1099@gmial.com"});
+        i.putExtra(Intent.EXTRA_SUBJECT, "Asunto del correo");
+        i.putExtra(Intent.EXTRA_TEXT, "Cuerpo del correo");
+        startActivity(Intent.createChooser(i, "Enviar correo"));
+    }
+
+
+
 }
